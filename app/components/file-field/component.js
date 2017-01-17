c49f2aa2a40d181c936239aac50346c72fb1f30a
Tim Evans <tim.c.evans@me.com>
2017-01-16 22:47:06 -0500

    style file-fields nicely

--- app/components/file-field/component.js
+++ app/components/file-field/component.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 const { get, set } = Ember;
+
 export default Ember.Component.extend({
+
+  classNames: ['file-field'],
+
   didReceiveAttrs() {
     let file = get(this, 'value');
     if (file) {
