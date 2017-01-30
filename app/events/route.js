4bb9128953aa46799d3361bba5205ab0cec91791
Tim Evans <tim.c.evans@me.com>
2017-01-29 22:28:38 -0500

    now we can create new events!! :tada:

--- app/events/route.js
+++ app/events/route.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 import moment from 'moment';
+import method from 'ember-service-methods/inject';
 
 const { get } = Ember;
 
 export default Ember.Route.extend({
+
+  uploadPhoto: method(),
+
   model() {
     return this.store.findAll('event', { reload: true }).then(function (events) {
       return events.sortBy('endsAt').reduce(function (E, event) {
@@ -18,8 +22,16 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    addEvent() {
-
+    addEvent(attributes) {
+      let event = this.store.createRecord('event', attributes);
+      return event.save().then((event) => {
+        return this.uploadPhoto(attributes.coverPhoto, {
+          event,
+          tags: ['cover-photo']
+        });
+      }).then(() => {
+        this.transitionTo('event.edit', event);
+      });
     }
   }
 });
