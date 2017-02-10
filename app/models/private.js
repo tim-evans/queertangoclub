78a2ec19f40d84c33a804e05718477150fb531f1
Tim Evans <tim.c.evans@me.com>
2017-02-10 09:23:15 -0500

    add basic privates search page

--- app/models/private.js
+++ app/models/private.js
@@ -1,5 +1,13 @@
 import DS from 'ember-data';
 
+const { attr, belongsTo } = DS;
+
 export default DS.Model.extend({
+  title: attr('string'),
+  description: attr('string'),
+  availability: attr(),
+  session: belongsTo('session'),
+  teacher: belongsTo('teacher'),
+  event: belongsTo('event')
 });
 
