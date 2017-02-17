268cbeabb37dd159fd5aa002e525be5bc13e559c
Tim Evans <tim.c.evans@me.com>
2017-02-16 19:58:06 -0500

    auto set the end date if start date is filled

--- app/components/new-event-dialog/component.js
+++ app/components/new-event-dialog/component.js
@@ -1,11 +1,18 @@
 import Ember from 'ember';
 
-const { get } = Ember;
+const { get, set, isBlank } = Ember;
 
 export default Ember.Component.extend({
   classNames: ['new-event-dialog'],
 
   actions: {
+    fillInEndsAt({ changeset }) {
+      let startsAt = get(changeset, 'startsAt');
+      let endsAt = get(changeset, 'endsAt');
+      if (isBlank(endsAt)) {
+        set(changeset, 'endsAt', startsAt);
+      }
+    },
     submit(model, changes) {
       return get(this, 'onsubmit')(changes).then(get(this, 'dismiss'));
     }
