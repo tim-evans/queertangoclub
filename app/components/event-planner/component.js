f936832f08d8e5653e4c43f4f08dae3429f6a575
Tim Evans <tim.c.evans@me.com>
2017-06-14 21:45:23 -0400

    create basic {{event-planner}} component

---
+++ app/components/event-planner/component.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+const { computed, computed: { reads } } = Ember;
+
+export default Ember.Component.extend({
+  classNames: ['event-planner'],
+  center: reads('events.firstObject.startsAt'),
+  hours: computed({
+    get() {
+      let hours = [];
+      for (let i = 0; i <= 24; i++) {
+        let hour = (i % 12) || 12;
+        if (i >= 12) {
+          hours.push(`${hour}pm`);
+        } else {
+          hours.push(`${hour}am`);
+        }
+      }
+      return hours;
+    }
+  })
+});
